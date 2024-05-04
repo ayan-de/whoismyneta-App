bbaed6168e024a0b8aa55e477167afa8f5667319
Ayan De <deayan252@gmail.com>
2024-05-05 02:40:01 +0530

    "header file modified"

--- app/src/main/java/com/example/whoismyneta/header/Header.kt
+++ app/src/main/java/com/example/whoismyneta/header/Header.kt
@@ -1,4 +1,25 @@
 package com.example.whoismyneta.header
 
-class Header {
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.whoismyneta.ui.theme.WhoIsMyNetaTheme
+
+@Composable
+fun Header(
+
+) {
+    Column {
+        Text(text = "Who is My Neta")
+        Text(text = "Explore self-reported details and parliamentary activity of MPs in the 17th Lok Sabha")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HeaderPreview(){
+    WhoIsMyNetaTheme {
+        Header()
+    }
 }
