b59d10845d5ebfffcffbbfc899eaa504cd553307
Ayan De <deayan252@gmail.com>
2024-05-05 03:40:29 +0530

    "typography added to Header.kt file"

--- app/src/main/java/com/example/whoismyneta/header/Header.kt
+++ app/src/main/java/com/example/whoismyneta/header/Header.kt
@@ -1,6 +1,7 @@
 package com.example.whoismyneta.header
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
@@ -11,14 +12,20 @@ fun Header(
 
 ) {
     Column {
-        Text(text = "Who is My Neta")
-        Text(text = "Explore self-reported details and parliamentary activity of MPs in the 17th Lok Sabha")
+        Text(
+            text = "Who is My Neta",
+            style = MaterialTheme.typography.displayLarge
+        )
+        Text(
+            text = "Explore self-reported details and parliamentary activity of MPs in the 17th Lok Sabha",
+            style = MaterialTheme.typography.bodyLarge
+        )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun HeaderPreview(){
+fun HeaderPreview() {
     WhoIsMyNetaTheme {
         Header()
     }
