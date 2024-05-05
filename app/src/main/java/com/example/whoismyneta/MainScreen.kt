6cdbca4035b29b3e1e62007c41efbd695d65ec9a
Ayan De <deayan252@gmail.com>
2024-05-05 16:39:07 +0530

    "LazyColumn added for main screen"

--- app/src/main/java/com/example/whoismyneta/MainScreen.kt
+++ app/src/main/java/com/example/whoismyneta/MainScreen.kt
@@ -1,11 +1,16 @@
 package com.example.whoismyneta
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.add
 import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.whoismyneta.header.Header
@@ -19,6 +24,16 @@ fun MainScreen(){
                 .systemBars
                 .add(WindowInsets(left = 16.dp, top = 16.dp, right = 16.dp, bottom = 16.dp))
                 .asPaddingValues()
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentPadding = contentPadding,
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                item {
+                    Header()
+                }
+            }
         }
     }
 }
