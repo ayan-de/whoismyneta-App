7ae7d000e233669f2039831b199e03c78c9042c5
Ayan De <deayan252@gmail.com>
2024-05-05 17:02:38 +0530

    "window compat added"

--- app/src/main/java/com/example/whoismyneta/MainActivity.kt
+++ app/src/main/java/com/example/whoismyneta/MainActivity.kt
@@ -10,11 +10,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import com.example.whoismyneta.ui.theme.WhoIsMyNetaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window,false)
+
         setContent {
             WhoIsMyNetaTheme {
                 // A surface container using the 'background' color from the theme
